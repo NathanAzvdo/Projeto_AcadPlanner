bc4699963a769fcaaf0192e7b19d53ea7e16b47b
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-01-25 14:59:19 -0300

    fist commit

---
+++ src/main/java/com/NathanAzvdo/AcadPlanner/AcadPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.NathanAzvdo.AcadPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NathanAzvdo/AcadPlanner/AcadPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NathanAzvdo.AcadPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
