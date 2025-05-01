62866038d035d33a43cbae1d726b1f9af6e17792
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-05-01 20:31:42 -0300

    feat: rotas admin/user configured

--- src/main/java/com/NathanAzvdo/AcadPlanner/config/SecurityConfigurations.java
+++ src/main/java/com/NathanAzvdo/AcadPlanner/config/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.NathanAzvdo.AcadPlanner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,10 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/materia", "/curso").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 
